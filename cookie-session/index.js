66a86c27652ebd9bee7b252bb5f7aa73be5eca2e
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-04-14 13:17:54 +0700

    cookie session

--- cookie-session/index.js
+++ cookie-session/index.js
@@ -1,1 +1,17 @@
-const cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session');
+const express = require('express');
+
+const app = express();
+
+app.use(cookieSession({ secret: 'ichsan is cool' }));
+
+app.use(count);
+
+function count(req, res) {
+    req.session.count = (req.session.count || 0) + 1;
+    res.send('viewed ' + req.session.count + ' times\n');
+};
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
